0d60512c79adb9b0fe70514d50138390bcd5550a
Adrien Siebert <asiebert@work4labs.com>
2017-10-24 13:48:31 -0700

    Revert "remove support for EmbeddedListField"
    This reverts commit 96d2435ace36604bec2ee2efc3b7194c207c4384. Reason for suppression is unknown, and we need it in a project.

--- tastypie_mongoengine/fields.py
+++ tastypie_mongoengine/fields.py
@@ -138,6 +138,104 @@ class EmbeddedDocumentField(BuildRelatedMixin, fields.ToOneField):
         return bundle.obj
 
 
+class EmbeddedListField(BuildRelatedMixin, fields.ToManyField):
+    """
+    Represents a list of embedded objects. It must be used in conjunction
+    with EmbeddedDocumentField.
+    """
+
+    is_related = False
+    is_m2m = False
+
+    def __init__(self, of, attribute, **kwargs):
+        self._to_class_with_listresource = None
+
+        help_text = kwargs.pop('help_text', None)
+
+        super(EmbeddedListField, self).__init__(to=of, attribute=attribute, **kwargs)
+
+        self._help_text = help_text
+
+    @property
+    def help_text(self):
+        if not self._help_text:
+            self._help_text = "List of embedded documents (%s)." % (self.to_class(self.get_api_name())._meta.resource_name,)
+        return self._help_text
+
+    def build_schema(self):
+        data = {
+            'embedded': {
+                'fields': self.to_class(self.get_api_name()).build_schema()['fields'],
+            },
+            'related_type': 'to_many',
+        }
+
+        type_map = getattr(self.to_class(self.get_api_name())._meta, 'polymorphic', {})
+        if not type_map:
+            return data
+
+        data['embedded'].update({
+            'resource_types': type_map.keys(),
+        })
+
+        return data
+
+    def dehydrate(self, bundle, for_list=True):
+        assert bundle.obj
+
+        the_m2ms = None
+
+        if isinstance(self.attribute, basestring):
+            the_m2ms = getattr(bundle.obj, self.attribute)
+        elif callable(self.attribute):
+            the_m2ms = self.attribute(bundle)
+
+        if not the_m2ms:
+            if not self.null:
+                raise exceptions.ApiFieldError("The document %r has an empty attribute '%s' and does not allow a null value." % (bundle.obj, self.attribute))
+            return []
+
+        self.m2m_resources = []
+        m2m_dehydrated = []
+
+        # the_m2ms is a list, not a queryset
+        for index, m2m in enumerate(the_m2ms):
+            m2m.parent = bundle.obj
+            m2m_resource = self.get_related_resource(m2m)
+
+            pk_field = getattr(m2m_resource._meta, 'id_field', None)
+            if pk_field is None:
+                m2m.pk = index
+            else:
+                m2m.__class__.pk = link_property(pk_field)
+
+            m2m_bundle = tastypie_bundle.Bundle(obj=m2m, request=bundle.request)
+            self.m2m_resources.append(m2m_resource)
+            if tastypie.__version__ >= (0, 9, 15):
+                m2m_dehydrated.append(self.dehydrate_related(m2m_bundle, m2m_resource, for_list=for_list))
+            else:
+                m2m_dehydrated.append(self.dehydrate_related(m2m_bundle, m2m_resource))
+
+        return m2m_dehydrated
+
+    def hydrate(self, bundle):
+        return [b.obj for b in self.hydrate_m2m(bundle)]
+
+    @property
+    def to_class(self):
+        if not self._to_class_with_listresource:
+            # Importing here to prevent import cycle
+            from tastypie_mongoengine import resources
+            base = super(EmbeddedListField, self).to_class
+            # We create a new ad-hoc resource class here, mixed with MongoEngineListResource, pretending to be original class
+            self._to_class_with_listresource = type(base.__name__, (base, resources.MongoEngineListResource), {
+                '__module__': base.__module__,
+                '_parent': self._resource,
+                'attribute': self.attribute or self.instance_name,
+            })
+        return self._to_class_with_listresource
+
+
 class ReferencedListField(TastypieMongoengineMixin, fields.ToManyField):
     """
     Represents a list of referenced objects. It must be used in conjunction
